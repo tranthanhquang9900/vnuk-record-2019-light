bb23798c1bfed080d930f9f1300f00b3d0500ad1
Modern Coding <cormorant.programming@gmail.com>
2018-01-31 11:39:54 +0700

    Servlets

---
+++ src/vn/edu/vnuk/record/servlet/HiWorld.java
@@ -0,0 +1,27 @@
+package vn.edu.vnuk.record.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HiWorld extends HttpServlet {
+	
+	@Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>First Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hi World Servlet on Eclipse!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
