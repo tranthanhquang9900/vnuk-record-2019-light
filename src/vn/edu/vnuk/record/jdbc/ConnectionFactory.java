17c3cd3cae3a4e4a2721bfe03061f92aa747fe76
Modern Coding <cormorant.programming@gmail.com>
2019-04-03 19:37:50 +0700

    Deployment Assembly

--- src/vn/edu/vnuk/record/jdbc/ConnectionFactory.java
+++ src/vn/edu/vnuk/record/jdbc/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	
 	public Connection getConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(
 				"jdbc:mysql://localhost/vnuk", 
 				"root", 
